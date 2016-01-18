8c4ecc2e7cff96cd2e1e7092cd74e0dde57c3686
Sudaemon <sudaemon@ya.ru>
2016-01-19 02:49:59 +0300

    Fixed commented passenger deb repo addition ))

--- image/enable_repos.sh
+++ image/enable_repos.sh
@@ -9,11 +9,11 @@ set -x
 
 	## ------- Phusion Passenger
 
-# if [[ "$PASSENGER_ENTERPRISE" ]]; then
-# 	echo deb https://download:$PASSENGER_ENTERPRISE_DOWNLOAD_TOKEN@www.phusionpassenger.com/enterprise_apt trusty main > /etc/apt/sources.list.d/passenger.list
-# else
-# 	echo deb https://oss-binaries.phusionpassenger.com/apt/passenger trusty main > /etc/apt/sources.list.d/passenger.list
-# fi
+if [[ "$PASSENGER_ENTERPRISE" ]]; then
+	echo deb https://download:$PASSENGER_ENTERPRISE_DOWNLOAD_TOKEN@www.phusionpassenger.com/enterprise_apt trusty main > /etc/apt/sources.list.d/passenger.list
+else
+	echo deb https://oss-binaries.phusionpassenger.com/apt/passenger trusty main > /etc/apt/sources.list.d/passenger.list
+fi
 
 	## ------- Rowan's Redis PPA
 
