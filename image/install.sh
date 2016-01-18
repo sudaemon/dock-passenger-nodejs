acef9e1f1fd38ef6c9eb21c2fac417242831083c
Sudaemon <sudaemon@ya.ru>
2016-01-19 01:52:47 +0300

    All the Ruby stuff's commented out. Node.js version changed to 4.x. Phusion passenger left installed.

--- image/install.sh
+++ image/install.sh
@@ -7,15 +7,18 @@ set -x
 /pd_build/prepare.sh
 /pd_build/utilities.sh
 
-if [[ "$ruby19" = 1 ]]; then /pd_build/ruby1.9.sh; fi
-if [[ "$ruby20" = 1 ]]; then /pd_build/ruby2.0.sh; fi
-if [[ "$ruby21" = 1 ]]; then /pd_build/ruby2.1.sh; fi
-if [[ "$ruby22" = 1 ]]; then /pd_build/ruby2.2.sh; fi
-if [[ "$jruby90" = 1 ]]; then /pd_build/jruby9.0.sh; fi
-if [[ "$python" = 1 ]]; then /pd_build/python.sh; fi
-if [[ "$nodejs" = 1 ]]; then /pd_build/nodejs.sh; fi
-if [[ "$redis" = 1 ]]; then /pd_build/redis.sh; fi
-if [[ "$memcached" = 1 ]]; then /pd_build/memcached.sh; fi
+# if [[ "$ruby19" = 1 ]]; then /pd_build/ruby1.9.sh; fi
+# if [[ "$ruby20" = 1 ]]; then /pd_build/ruby2.0.sh; fi
+# if [[ "$ruby21" = 1 ]]; then /pd_build/ruby2.1.sh; fi
+# if [[ "$ruby22" = 1 ]]; then /pd_build/ruby2.2.sh; fi
+# if [[ "$jruby90" = 1 ]]; then /pd_build/jruby9.0.sh; fi
+# if [[ "$python" = 1 ]]; then /pd_build/python.sh; fi
+# if [[ "$nodejs" = 1 ]]; then /pd_build/nodejs.sh; fi
+# if [[ "$redis" = 1 ]]; then /pd_build/redis.sh; fi
+# if [[ "$memcached" = 1 ]]; then /pd_build/memcached.sh; fi
+
+# install node.js
+/pd_build/nodejs.sh
 
 # Must be installed after Ruby, so that we don't end up with two Ruby versions.
 /pd_build/nginx-passenger.sh
