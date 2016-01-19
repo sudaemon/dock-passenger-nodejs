a16df552bb02f2d23ea054ac103cda332bfe1323
Sudaemon <sudaemon@ya.ru>
2016-01-19 23:56:40 +0300

    Test commit to trigger docker hub build

--- image/nginx-passenger.sh
+++ image/nginx-passenger.sh
@@ -10,6 +10,7 @@ if [[ "$PASSENGER_ENTERPRISE" ]]; then
 else
 	apt-get install -y nginx-extras passenger --force-yes
 fi
+
 cp /pd_build/config/30_presetup_nginx.sh /etc/my_init.d/
 cp /pd_build/config/nginx.conf /etc/nginx/nginx.conf
 mkdir -p /etc/nginx/main.d
