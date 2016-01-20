b952e1cc0801555a4ab656e0fc4dd5ce03b64370
Sudaemon <sudaemon@ya.ru>
2016-01-20 22:55:13 +0300

    Everything but node removed

--- image/nginx-passenger.sh
+++ image/nginx-passenger.sh
@@ -5,6 +5,7 @@ source /etc/environment
 set -x
 
 ## Install Phusion Passenger.
+
 if [[ "$PASSENGER_ENTERPRISE" ]]; then
 	apt-get install -y nginx-extras passenger-enterprise
 else
@@ -17,27 +18,10 @@ mkdir -p /etc/nginx/main.d
 cp /pd_build/config/nginx_main_d_default.conf /etc/nginx/main.d/default.conf
 
 ## Install Nginx runit service.
+
 mkdir /etc/service/nginx
 cp /pd_build/runit/nginx /etc/service/nginx/run
 touch /etc/service/nginx/down
 
 mkdir /etc/service/nginx-log-forwarder
 cp /pd_build/runit/nginx-log-forwarder /etc/service/nginx-log-forwarder/run
-
-## Precompile Ruby extensions.
-# if [[ -e /usr/bin/ruby2.1 ]]; then
-# 	ruby2.1 -S passenger-config build-native-support
-# 	setuser app ruby2.1 -S passenger-config build-native-support
-# fi
-# if [[ -e /usr/bin/ruby2.0 ]]; then
-# 	ruby2.0 -S passenger-config build-native-support
-# 	setuser app ruby2.0 -S passenger-config build-native-support
-# fi
-# if [[ -e /usr/bin/ruby1.9.1 ]]; then
-# 	ruby1.9.1 -S passenger-config build-native-support
-# 	setuser app ruby1.9.1 -S passenger-config build-native-support
-# fi
-# if [[ -e /usr/local/jruby-1.7.18/bin/jruby ]]; then
-#   jruby -S passenger-config build-native-support
-#   setuser app jruby -S passenger-config build-native-support
-# fi
--- image/python.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-source /pd_build/buildconfig
-set -x
-
-## Install Python.
-apt-get install -y python python2.7 python3
